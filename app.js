0f69074c8bcea828af5e1e1ca993cf992ebe7f2f
sadeeshbabu <sadeeshbabu.kga@gmail.com>
2019-07-29 04:38:50 +0100

    Fifth

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-var msg = 'Hello World';
-console.log(msg);
+var http = require('http');
+
+//create a server object:
+http.createServer(function (req, res) {
+  res.write('Hello World!'); //write a response to the client
+  res.end(); //end the response
+  var msg = 'Hello World';
+  console.log(msg);
+}).listen(8080); //the server object listens on port 8080
+
